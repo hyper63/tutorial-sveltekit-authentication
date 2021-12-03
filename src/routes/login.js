624dd42e288ca01b8a8925e586baa84ea01f1217
mattiasinokuchi <8-b@tuta.io>
2021-12-03 15:36:42 +0100

    added comments

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,8 +1,11 @@
+//  Login endpoint
+
 const ghAuthURL = 'https://github.com/login/oauth/authorize'
 const clientId = import.meta.env.VITE_CLIENT_ID
 
 export async function get(req) {
   const sessionId = '1234'
+  //  Redirect request to GitHub authentication endpoint with CLIENT_ID
   return {
     status: 302,
     headers: {
